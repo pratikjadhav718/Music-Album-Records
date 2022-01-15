f7e5b1877be2af88ebc6a9d5ee129c2e91318931
pratikjadhav718 <pratikjadhav718@gmail.com>
2022-01-15 18:19:29 +0530

    Adding Context

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({children}) =>{
+    const [token, setToken] = useState("");
+    const handleToken = (newToken)  => {
+        setToken(newToken);
+    };
+
+    return(
+
+        <AuthContext.Provider value={{token, handleToken}}>{children}</AuthContext.Provider>
+    )
+}
