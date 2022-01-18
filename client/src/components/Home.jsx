85454e31c3bb0347217b2972f5c9b82d8321aa1d
pratikjadhav718 <pratikjadhav718@gmail.com>
2022-01-18 10:32:14 +0530

    18-01-2022

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,34 +2,25 @@ import "./home.css";
 import { DisplayAlbum } from "./DisplayAlbum/DisplayAlbum"
 import { Footer } from "./footer";
 import { useState } from "react";
+import { SearchBar } from "./SearchBar/SearchBar";
 
 export const Home = () => {
     const [pageNo, setpageNo] = useState(1);
 
     return (
         <div>
+            <SearchBar />
             
-            
-            <h1> Home </h1>
+            {/* <h1> Home </h1> */}
             <div  className="container-div-for-display-album">
 
                 <DisplayAlbum />
                 <DisplayAlbum />
                 <DisplayAlbum />
                 <DisplayAlbum />
-                <DisplayAlbum />
-                <DisplayAlbum />
 
-                <DisplayAlbum />
-                <DisplayAlbum />
-                <DisplayAlbum />
-                <DisplayAlbum />
-                <DisplayAlbum />
-                <DisplayAlbum />
-                <DisplayAlbum />
-                <DisplayAlbum />
-                <DisplayAlbum />
-                <DisplayAlbum />
+                {/* <DisplayAlbum />
+                <DisplayAlbum /> */}
             </div>
             <Footer pageNo={pageNo}/>
         </div>
---
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import "./searchbar.css";
+
+export const SearchBar = () => {
+    return (
+        <div className="searchbar-main-div">
+            <div className="searchbar-div">
+                <input type="text" />
+            </div>
+        </div>
+    )
+}
