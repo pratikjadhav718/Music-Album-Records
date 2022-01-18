67ed7ff37afe096389fd11ea28f1ff3e30cc91ec
pratikjadhav718 <pratikjadhav718@gmail.com>
2022-01-18 10:47:53 +0530

    adding fuctionalityes to frontend

--- client/src/components/DisplayAlbum/DisplayAlbum.jsx
+++ client/src/components/DisplayAlbum/DisplayAlbum.jsx
@@ -1,13 +1,26 @@
 import "./displayalbum.css";
 import coverpic from "./coverpic.jpg";
 
-export const DisplayAlbum = () => {
+export const DisplayAlbum = ({showData}) => {
+    console.log('showData:', showData)
     return (
-        <div className="display-album-main-div">
-            <h2>album name here</h2>
-            <img src={coverpic} alt="" />
-            <h3> Arijit Singh.</h3>
-            <p> <span>10</span> songs</p>
-        </div>
+        <>
+            {
+                showData.map((e) =>(
+                    <div className="display-album-main-div" key={e.ids}>
+                        <h2>{e.album_name}</h2>
+                        <img src={coverpic} alt="" />
+                        <h3>{e.artist_name}</h3>
+                        <p> <span>{e.song_count}</span> songs</p>
+                    </div>
+                ))
+            }
+            {/* <div className="display-album-main-div">
+                <h2>album name here</h2>
+                <img src={coverpic} alt="" />
+                <h3> Arijit Singh.</h3>
+                <p> <span>10</span> songs</p>
+            </div> */}
+        </>
     )
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,6 +7,13 @@ import { SearchBar } from "./SearchBar/SearchBar";
 export const Home = () => {
     const [pageNo, setpageNo] = useState(1);
 
+    const [showData, setShowData] = useState([
+        {album_name:"Sour", ids:1, image_link:"", artist_name:"Michael Jackson",song_count:10, description:"clear"}, 
+        {album_name:"Sour", ids:2, image_link:"", artist_name:"Michael Jackson",song_count:10, description:"clear"}, 
+        {album_name:"Sour", ids:3, image_link:"", artist_name:"Michael Jackson",song_count:10, description:"clear"}, 
+        {album_name:"Sour", ids:4, image_link:"", artist_name:"Michael Jackson",song_count:10, description:"clear"}
+    ])
+
     return (
         <div>
             <SearchBar />
@@ -14,10 +21,10 @@ export const Home = () => {
             {/* <h1> Home </h1> */}
             <div  className="container-div-for-display-album">
 
+                <DisplayAlbum showData={showData}/>
+                {/* <DisplayAlbum />
                 <DisplayAlbum />
-                <DisplayAlbum />
-                <DisplayAlbum />
-                <DisplayAlbum />
+                <DisplayAlbum /> */}
 
                 {/* <DisplayAlbum />
                 <DisplayAlbum /> */}
